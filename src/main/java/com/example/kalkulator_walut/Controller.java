9661b47b6b06d8f51839ba18b7c65973b982af16
KamilSkorski <95830158+KamilSkorski@users.noreply.github.com>
2022-12-31 18:36:16 +0100

    Wypelnianie pol Label, Obliczenia

--- src/main/java/com/example/kalkulator_walut/Controller.java
+++ src/main/java/com/example/kalkulator_walut/Controller.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import java.util.ArrayList;
+import javafx.scene.control.TextField;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 public class Controller implements Initializable {
     private String[] calculateType = {"Kod waluty", "Nazwa waluty"};
@@ -24,7 +27,9 @@ public class Controller implements Initializable {
     @FXML
     private Label fromLabel;
     @FXML
-            private Label toLabel;
+    private Label toLabel;
+    @FXML
+    private TextField dataField;
 
     //Tables of currencies codes and names
     //private String[] currencyCode = {};
@@ -34,8 +39,13 @@ public class Controller implements Initializable {
 
     ArrayList<String> codes = new ArrayList<>();
     ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> mid = new ArrayList<>();
     ArrayList<Currency> currency;
     ApiClient client;
+
+    HashMap<String, String> codeMidMap = new HashMap<>();
+
+
 public void initialize(URL location, ResourceBundle resources) {
     //Initialize CalculatePicker
     calculatePicker.getItems().addAll(calculateType);
@@ -48,15 +58,36 @@ public void initialize(URL location, ResourceBundle resources) {
         fillArrayLists();
         fillComboBoxCodeStart(fromPicker, codes);
         fillComboBoxCodeStart(toPicker, codes);
+        setLabel(fromPicker,fromLabel);
+        setLabel(toPicker, toLabel);
+        setHashMap(mid, codes);
     } catch (Exception ex) {
         connectionStatusLabel.setText("Błąd połączenia");
     }
-
 }
-    @FXML
-    protected void buttonClick() throws IOException, InterruptedException {
 
-    resultLabel.setText(":)");
+private void setHashMap(ArrayList<String> mid, ArrayList<String> codes)
+{
+    for(int i = 0 ;i<mid.size(); i++)
+    {
+        codeMidMap.put(codes.get(i),mid.get(i));
+    }
+}
+
+    @FXML
+    protected void buttonClick() throws IOException, InterruptedException
+    {
+        try
+        {
+            Float number =calculateValue(fromLabel.getText(), toLabel.getText());
+            Float numberToLabel = Math.round(number*100)/100f;
+            resultLabel.setText(numberToLabel.toString());
+        }catch (Exception ex)
+        {
+            resultLabel.setText("Błąd danych");
+        }
+
+
 
     }
     private void fillArrayLists()
@@ -65,9 +96,11 @@ public void initialize(URL location, ResourceBundle resources) {
         {
             codes.add(curr.code);
             names.add(curr.currency);
+            mid.add(curr.mid);
         }
         codes.add("PLN");
         names.add("złoty polski");
+        mid.add("1");
     }
     //Fill Combobox during start application
     private void fillComboBoxCodeStart(ComboBox box, ArrayList<String> list)
@@ -100,9 +133,50 @@ public void initialize(URL location, ResourceBundle resources) {
             toPicker.getItems().clear();
             fillComboBoxCodeStart(toPicker,names);
         }
-        
-
     }
 
+    private void setLabel(ComboBox box, Label label)
+    {
+        String textToLabel = null;
+        for(int i =0 ; i<codes.size(); i++)
+        {
+            if(codes.get(i) == (String)box.getValue() || names.get(i) == (String)box.getValue() )
+            {
+                textToLabel = codes.get(i);
+                break;
+            }
+        }
+        label.setText(textToLabel);
+    }
+
+    @FXML
+    private void setFromLabel()
+    {
+        setLabel(fromPicker, fromLabel);
+    }
+    @FXML
+    private void setToLabel()
+    {
+        resultLabel.setText("");
+        setLabel(toPicker, toLabel);
+    }
+
+    private Float calculateValue(String currencyOne, String currencyTwo)
+    {
+        Float one;
+        Float two;
+        Float datafFromField;
+        Float result;
+
+        one = Float.parseFloat(codeMidMap.get(currencyOne));
+        two = Float.parseFloat(codeMidMap.get(currencyTwo));
+        datafFromField = Float.parseFloat(dataField.getText());
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(datafFromField);
+        result = (one*datafFromField)/two;
+
+        return result;
+    }
 
 }
--- src/main/java/com/example/kalkulator_walut/Currency.java
+++ src/main/java/com/example/kalkulator_walut/Currency.java
@@ -2,6 +2,12 @@ package com.example.kalkulator_walut;
 
 public class Currency
 {
+  public Currency(String a, String b, String c)
+  {
+    currency =a;
+    code = b;
+    mid = c;
+  }
   public String currency;
   public String code;
   public String mid;
